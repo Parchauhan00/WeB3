47b7768664e9b8405ecaf84e19d39963fb1b44a3
Prashant Chauhan <vipchauhan00@gmail.com>
2025-10-08 23:18:06 +0530

    dom manipulation

--- JavaScriPt/JS_Apna/Dom/script.js
+++ JavaScriPt/JS_Apna/Dom/script.js
@@ -1,3 +1,19 @@
 // alert("hello!")
 
-window.console.log("hello bolo")
+let heading =document.getElementById("heading"); //h1
+console.dir(heading);
+
+let btn = document.getElementsByClassName("btn-class")
+console.dir(btn);
+console.log(btn);
+
+// let parahs = document.getElementsByTagName("p");
+// console.dir(parahs);
+
+// Query selector
+
+let Firstelement = document.querySelector("p"); // return first matching element
+console.dir(Firstelement);
+
+let allElements = document.querySelectorAll("p"); // return all matching elements
+console.dir(allElements);
