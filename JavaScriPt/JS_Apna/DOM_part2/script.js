495f154cd08746c0f524ac377ca7bbfdf9ebdacc
Prashant Chauhan <vipchauhan00@gmail.com>
2025-10-09 23:57:28 +0530

    dom attribute

---
+++ JavaScriPt/JS_Apna/DOM_part2/script.js
@@ -0,0 +1,13 @@
+let div = document.querySelectorAll("div")
+console.dir(div);
+
+ let id= div.getAttribute(id);
+ console.log(id);
+
+ let para = document.querySelector('p');
+ console.log(para.getAttribute)
+
+ console.log(para.setAttribute("class", "new class"));
+
+ let div2 = document.querySelector("div");
+ div2.style;
